b7fd4a05e51f372b7f1e19983168965b2883a24b
Noah <001ac3@gmail.com>
2018-04-23 20:20:19 +1200

    First web upload

---
+++ starwarsload/config.php
@@ -0,0 +1,25 @@
+<?php
+
+//General
+$APIKey = "1D75AF37BC7B4142EE743DD3EEC14272"; // Go get your API here: http://steamcommunity.com/dev/apikey
+
+$load_name ="Star Wars Load"; // For your Browser Name
+$community_name ="Goodneighbor"; // Your community/server name. The one in the middle
+$community_url = "www.starwarsload.com"; // Url for your users to know where to go
+$intro = TRUE; // If you want to turn this off, type FALSE instead
+// Introduction
+$tagline = " A long time ago, in a server far,<br> far away...."; // The intro text in the beginning of the animation
+// Above Server Name
+$sub_title_1 = "Welcome"; // Above your server name
+// Under Server Name
+$sub_title_2 = "Star Wars Clone Wars RP"; // Below your server name
+
+//Bottom Part
+$left_bottom_title = "About Us";
+$left_bottom_sub_title = "$community_url for more info";
+$right_bottom_title = "Our Server";
+$right_bottom_sub_title = "General information about the server";
+
+$left_bottom_text = " ";
+
+?>
